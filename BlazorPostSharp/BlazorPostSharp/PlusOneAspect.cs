fdb0e9279cd337906e1ec6ba5fe050a9bcac015c
Honza Šinkora <honza@postsharp.net>
2020-02-06 15:42:28 +0100

    Add the projects

---
+++ BlazorPostSharp/BlazorPostSharp/PlusOneAspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using PostSharp.Aspects;
+using PostSharp.Aspects.Advices;
+using PostSharp.Serialization;
+
+namespace BlazorApp1.Shared
+{
+    [PSerializable]
+    public class PlusOneAspect : MethodLevelAspect
+    {
+        [OnMethodInvokeAdvice, SelfPointcut]
+        public void IncreaseValue([Argument(0)] int parameter, [ReturnValue] out int returnValue)
+        {
+            returnValue = parameter + 1;
+        }
+    }
+
+    [Serializable]
+    public class PlusOneAspectOld : MethodInterceptionAspect
+    {
+        public override void OnInvoke(MethodInterceptionArgs args)
+        {
+            args.ReturnValue = (int)args.Arguments[0] + 1;
+        }
+    }
+}
